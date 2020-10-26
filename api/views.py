1a9f85f33977932049a86069e19e1fca54e54c26
Максим Секачёв <cement-fan@ya.ru>
2020-10-26 18:56:01 +0300

    add viewset GroupViewSet

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import permissions
 from rest_framework import viewsets
 
-from .models import Post, Comment
+from .models import Post, Comment, Group
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer, GroupSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -28,3 +28,16 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Comment.objects.filter(post=self.kwargs['post_id'])
         return queryset
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+    # def perform_create(self, serializer):
+    #     serializer.save(author=self.request.user)
+
+    # def get_queryset(self):
+    #     queryset = Comment.objects.filter(post=self.kwargs['post_id'])
+    #     return queryset
