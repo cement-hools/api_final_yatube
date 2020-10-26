dca17beee2aae078290091ba41c91d2557be419e
Максим Секачёв <cement-fan@ya.ru>
2020-10-26 16:53:22 +0300

    add permissions.py

--- api/views.py
+++ api/views.py
@@ -1,1 +1,30 @@
-# TODO:  Напишите свой вариант
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from posts.models import Post, Comment
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,
+                          IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,
+                          IsAuthorOrReadOnly,)
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        queryset = Comment.objects.filter(post=self.kwargs['post_id'])
+        return queryset
--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -87,8 +87,11 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
-REST_FRAMEWORK = {        
-        'DEFAULT_PERMISSION_CLASSES': [
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
+        'DEFAULT_AUTHENTICATION_CLASSES': [
             'rest_framework_simplejwt.authentication.JWTAuthentication',
         ],
     }
